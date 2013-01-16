f0490094493548419597f1e4a53bce969a4721a9
Robert Viehauser <robert.viehauser@gmail.com>
2013-01-16 16:18:40 +0100

    get random patches

---
+++ implementation/dcp_get_random_patches.m
@@ -0,0 +1,37 @@
+function patches = dcp_get_random_patches(params, I, npatches)
+% extract npatches random quadratic patches
+% return: cell array of patches struct
+%         patches struct: data - the image data
+%                         rect - the patch rectangle of the image I
+
+[img_rows img_cols] = size(I);
+
+maxsz = min(params.pat_maxsz, min(img_rows, img_cols));
+
+if maxsz < params.pat_minsz
+    display('Image smaller than pat_minsz!')
+    patches = [];
+    return
+end
+
+r = rand(1,3*npatches);
+
+rand_idx = 1;
+patches = cell(1,npatches);
+for i = 1:npatches
+    % select random size
+    sz = params.pat_minsz + ceil(r(rand_idx) * (maxsz-params.pat_minsz));
+    
+    % select random top left corner
+    tl = [ceil(r(rand_idx+1) * (img_rows-sz)) ceil(r(rand_idx+2) * (img_cols-sz))];
+    br = tl + sz;
+    
+    % extract patch
+    patches{i}.data = I(tl(1):br(1), tl(2):br(2));
+    patches{i}.rect.tl = tl;
+    patches{i}.rect.sz = sz;
+    
+    rand_idx = rand_idx + 3;
+end
+
+end
--- implementation/dcp_load_params.m
+++ implementation/dcp_load_params.m
@@ -1,5 +1,12 @@
 function params = dcp_load_params
 
+% DCP Extract
+params.npatches = 10;
+params.one_patch_out_of = 2000;
+
+params.pat_maxsz = 300;
+params.pat_minsz = 80;
+
 % k-means specific
 params.kmeans_nclusterfactor = 4;
 params.kmeans_distance = 'l2';
