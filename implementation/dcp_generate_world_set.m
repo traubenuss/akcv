41e97ab3bd7d82f0644aa80ee1022b44eea31b59
Christoph Bichler <cbichler@cbichler-M70Vm.(none)>
2013-01-16 22:12:55 +0100

    added code for generating the world set

--- implementation/dcp_generate_world_set.m
+++ implementation/dcp_generate_world_set.m
@@ -14,3 +14,52 @@ if exist('world_set.mat') && ~overwrite
     world_set = load(world_set_filename, 'world_set');
     return;
 end
+
+fileList = getFilesInDirAndSubDirs(params.img_dir);
+numFiles = size(fileList,1);
+
+
+if(numFiles < params.n_images)
+    display('Less images for natural world set than required. Reducing number');
+    chosen_files = fileList;
+else
+    random_indxs = randperm(numFiles);
+    chosen_files = random_indxs(1:params.n_images);
+end
+
+%chosen_files is the set of indizes of the fileList vector chosen for
+%further computations
+
+chosen_files
+
+expected_patch_number = size(chosen_files,2) .* ((params.n_ppi_max + params.n_ppi_min)/2)
+
+hog_features_res = cell(1, expected_patch_number);
+hog_index = 1;
+
+%now, compute hog vectors for a random number of patches for every image
+for num=1:size(chosen_files,2)
+    %get the next image
+    I = imread(fileList{chosen_files(num)});
+    %get random number of patches
+    rand_patch_num = floor(params.n_ppi_min + rand(1,1) * (params.n_ppi_max - params.n_ppi_min))
+    patches = dcp_get_random_patches(params, I, rand_patch_num);
+    
+    %compute hog features for patches
+    for indx=1:size(patches,1)
+        cellSize = 8;
+        hog_img = patches{indx}.data;
+        
+        hog_res = vl_hog(hog_img, cellSize,'numOrientations', 8);
+        
+        if(hog_index < expected_patch_number)
+            hog_features_res{hog_index} = hog_res;
+        else
+            hog_features_res = {hog_features; hog_res};
+        end
+        hog_index = hog_index + 1; 
+    end
+end
+
+save(world_set_filename, hog_features);
+world_set = true;
---
+++ implementation/utils/getFilesInDirAndSubDirs.m
@@ -0,0 +1,18 @@
+function fileList = getFilesInDirAndSubDirs(dirName)
+
+  dirData = dir(dirName);     
+  dirIndex = [dirData.isdir];  
+  fileList = {dirData(~dirIndex).name}'
+  if ~isempty(fileList)
+    fileList = cellfun(@(x) fullfile(dirName,x),...  
+                       fileList,'UniformOutput',false);
+  end
+  subDirs = {dirData(dirIndex).name};  
+  validIndex = ~ismember(subDirs,{'.','..'});  
+                                             
+  for iDir = find(validIndex)                  
+    nextDir = fullfile(dirName,subDirs{iDir});   
+    fileList = [fileList; getFilesInDirAndSubDirs(nextDir)];  
+  end
+
+end
